4d18ad94bc195ce4a2266021729f4e5c1dfef90a
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-17 09:07:26 +0200

    feat(bonus 2): add generate button

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
     // Email list DOM element
     const emailListElement = document.getElementById('email-list');
 
+    // Generate button DOM element
+    const generateButtonElement = document.getElementById('generate-button');
+
 // #endregion Constants declaration
 
 
@@ -38,43 +41,70 @@
     }
     //#endregion print array function
 
+    // #region Generate emails function
+    function generateEmails(outputListElement) {
+
+        // Clear previous emails
+        outputListElement.innerHTML = '';
+
+        // Change generate button text
+        generateButtonElement.innerText = 'Generazione...';
+
+        // Disable generate button
+        generateButtonElement.disabled = true;
+
+        // Initialize an empty email list array
+        const emailList = [];
+
+        // Loop to reach desired emails number
+        for (let index = 0; index < EMAIL_NUMBER; index++) {
+
+            // Fetch a random email via API
+            fetch(EMAIL_API_URL)
+                // Get response
+                .then(response => response.json())
+                // Get data
+                .then(data => {
+
+                    // Get email from JSON
+                    const randomGeneratedEmail = data.response;
+
+                    // Push random generated email address into email list array
+                    emailList.push(randomGeneratedEmail);
+
+                    // IF all emails are generated
+                    if (emailList.length === EMAIL_NUMBER) {
+
+                        // Print email list array in its DOM element
+                        printArray(emailList, outputListElement);
+
+                        // Change generate button text
+                        generateButtonElement.innerText = 'Genera';
+
+                        // Enable generate button
+                        generateButtonElement.disabled = false;
+
+                    }
+
+                })
+                // Catch error
+                .catch(error => console.error(error));
+
+        }
+
+    }
+    // #endregion Generate emails function
+
 // #endregion Functions declaration
 
 
 
 // #region Script logic
 
-    // Initialize an empty email list array
-    const emailList = [];
+    // Generate emails on page load
+    generateEmails(emailListElement);
 
-    // Loop to reach desired emails number
-    for (let index = 0; index < EMAIL_NUMBER; index++) {
-
-        // Fetch a random email via API
-        fetch(EMAIL_API_URL)
-            // Get response
-            .then(response => response.json())
-            // Get data
-            .then(data => {
-
-                // Get email from JSON
-                const randomGeneratedEmail = data.response;
-
-                // Push random generated email address into email list array
-                emailList.push(randomGeneratedEmail);
-
-                // IF all emails are generated
-                if (emailList.length === EMAIL_NUMBER) {
-
-                    // Print email list array in its DOM element
-                    printArray(emailList, emailListElement);
-
-                }
-
-            })
-            // Catch error
-            .catch(error => console.error(error));
-
-    }
+    // Generate emails on button click
+    generateButtonElement.addEventListener('click', () => generateEmails(emailListElement));
 
 // #endregion Script logic
